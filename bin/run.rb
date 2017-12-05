c7931c33340050d347fcf2434c53481798d28d57
github username <github email address>
2017-12-05 13:33:16 -0500

    started runner method

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,12 @@
 require_relative 'config/environment'
 
+def runner
+  # Welcome the user and prompt for name
+  puts "Welcome to GooderBooks! Please enter your first name:"
+
+  # Get user input for name lookup
+  user_name = gets.chomp
+  
+  # Return or create user instance if none found
+  User.find_or_create_by(first_name: user_name)
+end
