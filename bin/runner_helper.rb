b3f064ad11d28ef2f886efc0e9d7feb5df56e6cc
github username <github email address>
2017-12-07 16:20:20 -0500

    test to override remote master

--- bin/runner_helper.rb
+++ bin/runner_helper.rb
@@ -17,13 +17,8 @@ end
 def home_screen(user)
   puts "Welcome #{user.first_name} #{user.last_name}"
   puts "Your commands options are"
-<<<<<<< HEAD
   puts "1. Start a new book"
   puts "2. Write a review"
-=======
-  puts "1. Start a book"
-  puts "2. Add a review"
->>>>>>> origin/testing-humzah
   puts "3. Edit a review"
   puts "4. Delete a review"
   puts "5. My reviews"
@@ -34,6 +29,7 @@ def home_screen(user)
   puts "10. Top 10 highest rated books by genre"
   puts "11. Get a book recommendation"
   puts "12. Quit application"
+  puts "THIS IS FOR TESTING PURPOSES"
 
   input = gets.chomp.to_i
   case input
