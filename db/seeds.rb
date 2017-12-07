491e5492b1ec7f71b9a24d059c5287b580f014aa
github username <github email address>
2017-12-07 11:24:07 -0500

    fully seeded reviews, users, books

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+#
 book_list = [
   ["The Great Gatsby", "F. Scott Fitzgerald", "Classic", 1925, "Book about people cheating on each other and then the girl gets hit by a car and the dude dies." ],
   ["Frankenstien", "Mary Shelly", "Horror", 1903, "EEERHHHHRRGGG" ],
@@ -36,6 +37,60 @@ user_list = [
   ["Eric", "Kollegger"], ["Humzah", "Choudrey"], ["Alana", "Mills"]
 ]
 
+review_list = [
+  [3, "This was the best thing i've read so far this year", 1, 1, nil, nil, "completed"],
+  [2, "Timothy is a killer fuelled by rejection, who murders teachers and keeps their eyebrows in a jar. Lady Snow, a farmer from Ohio, knows she has to stop him. Snow captures a killer, but it turns out the killer in custody is a copycat, and the real monster is still out there.", 2, 2, nil, nil, "completed"],
+  [4, "When a heiress from Sweden learns how to cure cancer, not everybody is supportive. However, her fortunes improve when her arch enemy buys a record company. However, the heiress finds change too difficult and goes back to being dissatisfied and grumpy.", 3, 3, nil, nil, "completed"],
+  [5, "A virgin from Wales is delighted when she gets the chance to take part in the final of a singing competition. However, her chances are scuppered when her car breaks down on the way to the competition. the virgin realises she's actually a ghost.", 4, 4, nil, nil, "completed"],
+  [5, "A solicitor from Australia is delighted when he gets the chance to take part in the final of a baking competition. However, his chances are scuppered when his mother is badly injured in a car accident. Finally, the solicitor makes it to the event, but is so flustered that he comes last.", 4, 5, nil, nil, "completed"],
+  [4, "A gardener from Ohio is delighted when she gets the chance to take part in the final of a pie eating contest. However, her chances are scuppered when her baby goes missing. Despite everything, the gardener wins the competition and becomes a global celebrity.", 5, 6, nil, nil, "completed"],
+  [1, "Judy is an acrobat from Mexico who falls in love with her teacher. The two are separated when the teacher accidentally runs over a child and becomes too distraught to love. Judy turns to food and becomes enormous then she accidentally falls on her teacher, crushing her.", 6, 7, nil, nil, "completed"],
+  [0, "In a world where zombies are rude and reckless, one electrician has no choice but to leave facebook using just his mind. Russell remembers that she actually saved the world ten years previous and is suffering from PTSD.", 7, 8, nil, nil, "completed"],
+  [0, "A vicar from Scotland is delighted when she gets the chance to take part in the final of Bake Off. However, her chances are scuppered when her breakfast is spiked. Despite the setback, the vicar comes second in the competition and vows to come back next year and win.", 8, 9, nil, nil, "completed"],
+  [2, "Maxwell is a banker from Alaska who falls in love with his step-sister. The two are separated when the step-sister accidentally runs over a rabbit and becomes too distraught to love. Maxwell turns to drink then accidentally runs his step-sister over.", 9, 10, nil, nil, "completed"],
+  [3, "Antoinette is a killer fuelled by homophobia, who murders teenagers and keeps their thumbs in a jar. Doctor Mendoza, an orphan from Sweden, knows she has to stop her. The story ends with Doctor Mendoza's mother's thumbs arriving in the post.", 10, 11, nil, nil, "completed"],
+  [2, "Deborah is a killer fuelled by lust, who murders prostitutes and keeps their eyebrows in a jar. Mrs McManus, a farmer from San Franciso, knows she has to stop her. The killer is never caught, leaving behind one of the greatest mysteries of all time.", 11, 12, nil, nil, "completed"],
+  [4, "Alastair is a billionaire from Ohio who falls in love with his tutor. The two are separated when the tutor falls for somebody else. Devestated by lost love, Alastair bursts into a police station with his tutor's head in a box.", 12, 13, nil, nil, "completed"],
+  [5, "When a business woman from Liverpool learns to sing, not everybody is supportive. However, her fortunes improve when her teacher buys a deralict castle. Eventually, the business woman manges to become a fugitive and earns the respect of her peers.", 13, 14, nil, nil, "completed"],
+  [4, "Ellen is a student from Maine who falls in love with her neighbour. The two are separated when the neighbour decides the relationship is inappropriate. However, Ellen manages to rescue the situation by slaying a dragon.", 14, 15, nil, nil, "completed"],
+  [3, "Melinda is a secretary from New York who falls in love with her gardener. The two are separated when the gardener goes away to sea. However, Melinda manages to rescue the situation by pretending to be somebody else.", 15, 16, nil, nil, "completed"],
+  [2, "Lilliana is a heiress from Maine who falls in love with her tutor. The two are separated when the tutor gets a horrifying tattoo. However, Lilliana manages to rescue the situation by robbing a bank.", 16, 17, nil, nil, "completed"],
+  [2, "In a world where women are rude and reckless, one politician has no choice but to leave facebook using just his mind. It turns out the politician exists only in the cloud.", 17, 18, nil, nil, "completed"],
+  [0, "Umair is a killer fuelled by love, who murders prostitutes and makes them into pies. DI Kassenaar, a waiter from New York, knows she has to stop him. Eventually, Kassenaar captures the killer just as he's about to board a private jet.", 18, 19, nil, nil, "completed"],
+  [1, "When a gentleman from Sweden learns to knit, not everybody is supportive. However, his fortunes improve when his best friend kills somebody. The gentleman later finds out that his best friend is the devil in diguise.", 19, 20, nil, nil, "completed"],
+  [2, "Vladimir is a killer fuelled by grief, who murders prostitutes and keeps their eyebrows in a jar. Doctor Bradley, a solicitor from Ohio, knows she has to stop him. The story ends with Doctor Bradley's brother's eyebrows arriving in the post.", 20, 21, nil, nil, "completed"],
+  [3, "When a cowboy from Japan decides to start eating people, not everybody is supportive. However, his fortunes improve when his step-sister buys a record company. Eventually, the cowboy realises that he has always been a worthwhile person and does not need to change.", 21, 22, nil, nil, "completed"],
+  [5, "Dominik is a killer fuelled by greed, who murders doctors and photographs his victims' lips. DCI Epperson, a geriatric from San Franciso, knows he has to stop him. The killer inspires multiple copycats across the globe, and the death count continues to rise, exponentially.", 22, 23, nil, nil, "completed"],
+  [4, "Catharina is a killer fuelled by lust, who murders nurses and posts photos of their tongues on Facebook. DCI Gogh, an author from Japan, knows he has to stop her. In a brutal conclusion, the killer murders Gogh's sister.", 23, 24, nil, nil, "completed"],
+  [4, "Lynda is a killer fuelled by hatred, who murders prostitutes and keeps their feet in a freezer. DCI Mortz, a Jedi from Canada, knows he has to stop her. The killer inspires multiple copycats across the globe, and the death count continues to rise, exponentially.", 24, 25, nil, nil, "completed"],
+  [2, "In a world where robots rule through fear, one dancer has no choice but to find another planet to colonise by killing her own father. Eventually, the dancer forms an army and teaches the robots a little respect.", 25, 1, nil, nil, "completed"],
+  [2, "When a billionaire from Liverpool learns to play guitar, not everybody is supportive. However, his fortunes improve when his tutor buys a record company. It turns out the billionaire is 200 years old but living off a life potion.", 26, 2, nil, nil, "completed"],
+  [1, "When an accountant from Australia develops a fear of insects, not everybody is supportive. However, his fortunes improve when his neighbour becomes a vet. Just as the accountant is about to achieve his goal, he's killed by a bus.", 27, 3, nil, nil, "completed"],
+  [0, "Indrani is an acrobat from Cornwall who falls in love with her golf caddy. The two are separated when the golf caddy falls into the ocean and loses her memory. Indrani turns to drink then accidentally runs her golf caddy over.", 28, 4, nil, nil, "completed"],
+  [3, "Maxim is a shepherd from England who falls in love with his golf caddy. The two are separated when the golf caddy catches the flu and has to stay at the home of strangers who become his new friends. It turns out Maxim and his golf caddy are the same person.", 29, 5, nil, nil, "completed"],
+  [0, "A vicar from Japan is delighted when he gets the chance to take part in the final of a baking competition. However, his chances are scuppered when his car breaks down on the way to the competition. Despite everything, the vicar wins the competition and becomes a global celebrity.", 30, 6, nil, nil, "completed"],
+  [4, "Demelza is a killer fuelled by racism, who murders women and posts their ears to their friends. Mrs Carlyle, a cleaner from Canada, knows she has to stop her. In a dramatic conclusion, Carlyle saves the killer from drowning, so that she can face the justice of the law.", 31, 7, nil, nil, "completed"],
+  [5, "A runner from Scotland is delighted when he gets the chance to take part in the final of X-Factor. However, his chances are scuppered when his car breaks down on the way to the competition. After the drama, the runner realises there is more to life than winning X-Factor and goes on a picninc with his father instead.", 32, 8, nil, nil, "completed"],
+  [5, "When a carpenter from Cornwall learns to sword fight, not everybody is supportive. However, her fortunes improve when her best friend kills somebody. Just as the carpenter is about to achieve her goal, she's killed by a bus.", 33, 9, nil, nil, "completed"],
+  [3, "A gardener from Ohio is delighted when she gets the chance to take part in the final of a talent show. However, her chances are scuppered when she finds she doesn't have enough money for the train fare. Distraught at losing a talent show, the gardener packers herself off to the mountains and beomes a hermit.", 34, 10, nil, nil, "completed"],
+  [5, "Lady is a killer fuelled by rejection, who murders children and keeps their fingers in a freezer. Doctor Meadows, a nurse from San Franciso, knows she has to stop her. It turns out the killer and Doctor Meadows are the same person.", 35, 11, nil, nil, "completed"],
+  [4, "Cornelius is a killer fuelled by homophobia, who murders doctors and keeps their tongues in a freezer. Detective Mackall, a lawyer from Denmark, knows she has to stop him. The killer is never caught, leaving behind one of the greatest mysteries of all time.", 36, 12, nil, nil, "completed"],
+  [4, "Nina is a nun from Ohio who falls in love with her golf caddy. The two are separated when the golf caddy catches the flu and has to stay in hospital where she makes new friends. It turns out her golf caddy is simply an action figure.", 37, 13, nil, nil, "completed"],
+  [2, "Evan is a virgin from Alaska who falls in love with his arch enemy. The two are separated when the arch enemy catches the flu and has to stay at the home of strangers who become his new friends. However, Evan manages to rescue the situation by changing his identity.", 38, 14, nil, nil, "completed"],
+  [0, "When a queen from Wales decides to start eating people, not everybody is supportive. However, her fortunes improve when her gardener recovers from cancer. In the end, the queen becomes a better person and is admired by all.", 39, 15, nil, nil, "completed"],
+  [1, "Jack is an orphan from Cornwall who falls in love with his teacher. The two are separated when the teacher gets a horrifying tattoo. However, Jack manages to rescue the situation by saving the life of his his cousin.", 40, 16, nil, nil, "completed"],
+  [1, "A sailor from Liverpool is delighted when he gets the chance to take part in the final of Britain's Got Talent. However, his chances are scuppered when his gradmother goes missing. Although the sailor doesn't win the competition, he realises who his true friends are and continues life with a healthy perspective.", 41, 17, nil, nil, "completed"],
+  [0, "A rower from Japan is delighted when she gets the chance to take part in the final of a singing competition. However, her chances are scuppered when she finds out her arch rival is also going to compete. Although the rower doesn't win the competition, she realises who her true friends are and continues life with a healthy perspective.", 42, 18, nil, nil, "completed"],
+  [3, "Carloine is a writer from Scotland who falls in love with her tutor. The two are separated when the tutor goes away to war. However, Carloine manages to rescue the situation by booking a wonderful restaurant.", 43, 19, nil, nil, "completed"],
+  [2, "Fin is an accountant from Denmark who falls in love with his gardener. The two are separated when the gardener goes away to war. However, Fin manages to rescue the situation by slaying a dragon.", 44, 20, nil, nil, "completed"],
+  [4, "Nikolas is a killer fuelled by racism, who murders old people and wraps them in toilet paper. Detective Sousa, an electrician from Australia, knows she has to stop her. It turns out the killer is simply an action figure.", 45, 21, nil, nil, "completed"],
+  [4, "Shirani is a killer fuelled by jealousy, who murders teenagers and posts photos of their fingers on Facebook. Mr Eden, a vicar from England, knows she has to stop her. It turns out the killer has been in jail all along, and is writing a story about an alternative life.", 46, 22, nil, nil, "completed"],
+  [2, "Eddy is a killer fuelled by hatred, who murders women and photographs her victims' lips. DI Hoisington, a stud from Alaska, knows she has to stop her. It turns out the killer is actually a rabbit.", 47, 23, nil, nil, "completed"],
+  [3, "When an electrician from New York learns to play the drums, not everybody is supportive. However, his fortunes improve when his tutor falls in love. It turns out the whole plot was a practical joke.", 48, 24, nil, nil, "completed"],
+  [1, "Ronda is a doctor from Ohio who falls in love with her arch enemy. The two are separated when the arch enemy goes away to sea. It turns out her arch enemy murdered the arch enemy's baby.", 49, 25, nil, nil, "completed"],
+  [1, "Zain is a killer fuelled by regret, who murders women and keeps their fingers in a freezer. Mr Myandee, an electrician from Mexico, knows she has to stop him. The killer is never caught, leaving behind one of the greatest mysteries of all time.", 50, 1, nil, nil, "completed"]
+]
+
 
 book_list.each do |title, author, genre, year, description|
   Book.create(
@@ -51,5 +106,17 @@ user_list.each do |first_name, last_name|
   User.create(
   first_name: first_name,
   last_name: last_name
+  )
+end
+
+review_list.each do |rating, description, user_id, book_id, start_date, end_date, status|
+  Review.create(
+  rating: rating,
+  description: description,
+  user_id: user_id,
+  book_id: book_id,
+  start_date: start_date,
+  end_date: end_date,
+  status: status
   )
 end
