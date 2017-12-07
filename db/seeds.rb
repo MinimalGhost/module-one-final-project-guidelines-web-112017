fd749d8f46c913fc8d346d3dd516d6f3f32b4504
github username <github email address>
2017-12-07 10:28:22 -0500

    erased all tables, added more books and names to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,46 @@
 book_list = [
-  [ "The Great Gatsby", "F. Scott Fitzgerald", "Classic", 1925, "Book about people cheating on each other and then the girl gets hit by a car and the dude dies." ],
-  [ "Frankenstien", "Mary Shelly", "Horror", 1903, "EEERHHHHRRGGG" ],
-  [ "To Kill a Mockingbird", "Harper Lee", 1945, "I think its about racism or something" ],
-  [ "1984", "George Orwell", "Classic", 1984, "This is not a book about big goverment. The goverment is great. I love goverment" ],
+  ["The Great Gatsby", "F. Scott Fitzgerald", "Classic", 1925, "Book about people cheating on each other and then the girl gets hit by a car and the dude dies." ],
+  ["Frankenstien", "Mary Shelly", "Horror", 1903, "EEERHHHHRRGGG" ],
+  ["To Kill a Mockingbird", "Harper Lee", 1945, "I think its about racism or something" ],
+  ["1984", "George Orwell", "Classic", 1984, "This is not a book about big goverment. The goverment is great. I love goverment" ],
   ["IT", "Stephen King", "Horror", 1988, "I'm too scared to even find out what the book is about."],
   ["Lord of the Rings", "J.R.R. Tolkien", "Fantasy", 1940, "Epic journey set upen by Elijah Wood and Rudy and another guy. The pretty much have to get rid of some jewelry or the world will die. BUT you have to throw the ring into Mount Dooms fiery lava pit."],
   ["And then there were none", "Agatha Christie", "Mystery", 1910, "People get invited to a house party, then die."],
   ["Dracula", "Bram Stoker", "Horror", 1901, "He comes to suck your blood."],
   ["Cat in the Hat", "Dr. Suess", "Childrens", 1988, "A cat shows up and babysits kids. The cat is goofy."],
   ["Green eggs and Ham", "Dr. Suess", "Childrens", 1990, "A guy really tries to sell kids on eating green eggs and ham."],
-  ["James and the Giant Peach", "Rohl Dahl", "Childrens", 1985, "A kid gets trapped inside of a peach which goes to New York City or something. Also the clouds turn into monsters."]
+  ["James and the Giant Peach", "Rohl Dahl", "Childrens", 1985, "A kid gets trapped inside of a peach which goes to New York City or something. Also the clouds turn into monsters."],
+  ["Name of the Wind", "Patrick Rothfuss", "Fantasy", 2005, "A troubador is orphaned and disocvers his ability to wield magic and call on the power of the wind."],
+  ["Rule of the Bone", "Russell Banks", 1995, "Coming-of-age story about the 14-year-old American narrator, Chappie, later dubbed Bone (named for a tattoo that he gets), who, after having dropped out of school, turns to the guidance of a Rastafarian Jamaican migrant worker."],
+  ["Gardens of the Moon", "Stephen Erikson", "Fantasy", 1999, "The Malazan Empire simmers with discontent, bled dry by interminable warfare, bitter infighting and bloody confrontations. Even the imperial legions, long inured to the bloodshed, yearn for some respite. Yet Empress Laseen's rule remains absolute, enforced by her dread Claw assassins."],
+  ["The Tipping Point", "Malcolm Gladwell", "Non-fiction", 2000, "The tipping point is that magic moment when an idea, trend, or social behavior crosses a threshold, tips, and spreads like wildfire."],
+  ["Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "Fantasy", 1997, "Harry Potter has no idea how famous he is. That's because he's being raised by his miserable aunt and uncle who are terrified Harry will learn that he's really a wizard, just as his parents were. But everything changes when Harry is summoned to attend an infamous school for wizards, and he begins to discover some clues about his illustrious birthright."],
+  ["Harry Potter And The Chamber Of Secrets", "J.K. Rowling", "Fantasy", 1998, "The Dursleys were so mean that hideous that summer that all Harry Potter wanted was to get back to the Hogwarts School for Witchcraft and Wizardry. But just as he's packing his bags, Harry receives a warning from a strange, impish creature named Dobby who says that if Harry Potter returns to Hogwarts, disaster will strike."],
+  ["Deadhouse Gates", "Stephen Erikson", "Fantasy", 2006, "In the vast dominion of Seven Cities, in the Holy Desert Raraku, the seer Sha'ik and her followers prepare for the long-prophesied uprising known as the Whirlwind. Unprecedented in size and savagery, this maelstrom of fanaticism and bloodlust will embroil the Malazan Empire in one of the bloodiest conflicts it has ever known, shaping destinies and giving birth to legends."],
+  ["Into Thin Air", "Jon Krakauer", "Non-fiction", 1997, "The definitive account of the deadliest season in the history of Everest by the acclaimed Outside journalist and author of the bestselling Into the Wild. Taking the reader step by step from Katmandu to the mountain's deadly pinnacle, Krakauer has his readers shaking on the edge of their seat."],
+  ["Into the Wild", "Jon Krakauer", "Non-fiction", 1996, "In April 1992 a young man from a well-to-do family hitchhiked to Alaska and walked alone into the wilderness north of Mt. McKinley. His name was Christopher Johnson McCandless. He had given $25,000 in savings to charity, abandoned his car and most of his possessions, burned all the cash in his wallet, and invented a new life for himself. Four months later, his decomposed body was found by a moose hunter."],
+  ["Fahrenheit 451", "Ray Bradbury", "Fiction", 1953, "A future American society where books are outlawed and 'firemen' burn any that are found. The lead character is a fireman named Montag who becomes disillusioned with the role of censoring works and destroying knowledge, eventually quitting his job and joining a resistance group who memorize and share the world's greatest literary and cultural works."],
+  ["Do Androids Dream of Electric Sheep?", "Philip K. Dick", "Science Fiction", 1968, "The novel is set in a post-apocalyptic San Francisco, where Earth's life has been greatly damaged by nuclear global war. The main plot follows Rick Deckard, a bounty hunter who is tasked with 'retiring' (i.e. killing) six escaped Nexus-6 model androids, while a secondary plot follows John Isidore, a man of sub-par IQ who aids the fugitive androids."],
+  ["Hamlet", "William Shakespeare", "Tragedy", 1601, "Dramatises the revenge Prince Hamlet is called to wreak upon his uncle, Claudius, by the ghost of Hamlet's father, King Hamlet. Claudius had murdered his own brother and seized the throne, also marrying his deceased brother's widow."],
+  ["Don Quixote", "Miguel de Cervantes", "Fiction", 1605, "Alonso Quixano, a retired country gentleman in his fifties, lives in an unnamed section of La Mancha with his niece and a housekeeper. He has become obsessed with books of chivalry, and believes their every word to be true, despite the fact that many of the events in them are clearly impossible. Quixano eventually appears to other people to have lost his mind from little sleep and food and because of so much reading."],
+  ["Ulysses", "James Joyce", "Fiction", 1922, "Chronicles the peripatetic appointments and encounters of Leopold Bloom in Dublin in the course of an ordinary day, 16 June 1904."]
 ]
 
+user_list = [
+  ["James Freshley"], ["Andrew Yun"], ["Chris Jones"], ["Mike Jones"], ["Harry Wilson"], ["Kate Ulrich"], ["Maxwell Bellar"], ["Natalie Wilks"],
+  ["Frank DiLeo"], ["Matt Hope"], ["William McDonald"], ["Lisa Hu"], ["Mike Burnett"], ["Steve Jerry"], ["Alex Kende"], ["Sam Jones"],
+  ["Sam Dalsimer"], ["Nadir Ali"], ["Billy Gretch"], ["Charlie Gluckstern"], ["Phillis Arness"], ["Wilma Flintstone"], ["Alexis Yun"],
+  ["Jamie Seager"], ["Julia North"], ["Carol Alistar"], ["Rick Cedar"], ["Adam Voss"], ["Ross Rockafellow"], ["Michael Chapman"], ["Mike Norbeck"],
+  ["Sally Sitwell"], ["Tom Waits"], ["Ben Abelson"], ["James Witherspoon"], ["Zach Morris"], ["Eddie Tulman"], ["Richard Harris"], ["Mardee Singer"],
+  ["Samantha Leigh"], ["Alissa Kalin"], ["Emily English"], ["Ali Green"], ["Susan Sanchez"], ["Craig Gromley"], ["Bill Hader"], ["Vince Gilligan"],
+  ["Eric Kollegger"], ["Humzah Choudrey"], ["Alana Mills"]
+]
 
 
 book_list.each do |title, author, genre, year, description|
   Book.create(
   title: title,
-  author: author, 
+  author: author,
   genre: genre,
   year: year,
   description: description
